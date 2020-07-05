76f65ab5603782f81fe0a94cc62dd46be1d87db4
Fosty Fost <fostyfost@gmail.com>
2020-07-05 23:38:20 +0300

    🚋🚟🚅 Experiment with `next-page-transition`

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,9 +1,14 @@
 import React, { FC, ReactNode } from 'react'
 
+import { PageTransition } from 'next-page-transitions'
+
 import { Clock } from '../clock'
 import { Xkcd } from '../xkcd'
 import { Navigation } from './navigation'
 import { Dog } from '../dog'
+import { Loader } from './loader'
+
+const TIMEOUT = 400
 
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
@@ -12,7 +17,47 @@ const Layout: FC<{ children: ReactNode }> = ({ children }) => {
       <Clock />
       <Xkcd small />
       <Dog />
-      {children}
+      <PageTransition
+        skipInitialTransition
+        timeout={TIMEOUT}
+        classNames='page-transition'
+        loadingComponent={<Loader />}
+        loadingDelay={500}
+        loadingTimeout={{
+          enter: TIMEOUT,
+          exit: 0,
+        }}
+        loadingClassNames='loading-indicator'
+      >
+        {children}
+      </PageTransition>
+      <style jsx global>{`
+        .page-transition-enter {
+          opacity: 0;
+          transform: translate3d(0, 20px, 0);
+        }
+        .page-transition-enter-active {
+          opacity: 1;
+          transform: translate3d(0, 0, 0);
+          transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
+        }
+        .page-transition-exit {
+          opacity: 1;
+        }
+        .page-transition-exit-active {
+          opacity: 0;
+          transition: opacity ${TIMEOUT}ms;
+        }
+        .loading-indicator-appear,
+        .loading-indicator-enter {
+          opacity: 0;
+        }
+        .loading-indicator-appear-active,
+        .loading-indicator-enter-active {
+          opacity: 1;
+          transition: opacity ${TIMEOUT}ms;
+        }
+      `}</style>
     </>
   )
 }

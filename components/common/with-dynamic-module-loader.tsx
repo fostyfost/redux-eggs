b23705a2d1f8f30483d2662cfca984d7adbcbe57
Fosty Fost <fostyfost@gmail.com>
2020-07-25 21:20:53 +0300

    ␡␡␡ Remove modules correctly ␡␡␡

--- components/common/with-dynamic-module-loader.tsx
+++ components/common/with-dynamic-module-loader.tsx
@@ -1,20 +1,34 @@
 import { NextPage } from 'next'
-import React, { useCallback } from 'react'
+import React, { useCallback, useRef } from 'react'
 import { useStore } from 'react-redux'
+import { flatten } from 'redux-dynamic-modules-core/lib/Utils/Flatten'
 
 import { ModuleTuple, NextPageWithModules } from '../../store/contracts'
-import { ModuleStoreWithSagaTasks } from '../../store/saga-extension/contracts'
+import { ModuleStoreWithSagaTasks, SagaModule } from '../../store/saga-extension/contracts'
 import { AddedModulesCleanup } from './dynamic-module-loader'
 
 export const withDynamicModuleLoader = (
   Component: NextPage<any>,
   modules: ModuleTuple<any> = [],
 ): NextPageWithModules => {
+  let modulesToBeAdded: SagaModule[] = []
+
   const Wrapper: NextPageWithModules = props => {
     const store = useStore() as ModuleStoreWithSagaTasks
 
+    const isFirstRender = useRef(true)
+
+    if (isFirstRender.current) {
+      isFirstRender.current = false
+
+      if (!modulesToBeAdded.length) {
+        modulesToBeAdded = flatten(modules)
+      }
+    }
+
     const cleanup = useCallback(() => {
-      store.removeModules(modules)
+      store.removeModules(modulesToBeAdded)
+      modulesToBeAdded = []
     }, [store])
 
     return (
@@ -29,11 +43,13 @@ export const withDynamicModuleLoader = (
 
   Wrapper.displayName = `withDynamicModuleLoader(${Component.displayName || 'Component'})`
 
-  if (Component.getInitialProps) {
-    Wrapper.getInitialProps = async context => {
-      context.store.addModules(modules)
-      return Component.getInitialProps?.(context)
+  Wrapper.getInitialProps = async context => {
+    if (!modulesToBeAdded.length) {
+      modulesToBeAdded = flatten(modules)
+      context.store.addModules(modulesToBeAdded)
     }
+
+    return Component.getInitialProps?.(context) || {}
   }
 
   return Wrapper

e34d33c6a7fe8eeb25fbdfd5627c4a60200798ff
Fosty Fost <fostyfost@gmail.com>
2020-07-26 16:43:16 +0300

    Add `jest` and `redux-saga-test-plan` 🚀

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  collectCoverageFrom: ['**!/!*.{js,jsx,ts,tsx}', '!**!/!*.d.ts', '!**!/node_modules/!**'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
+}

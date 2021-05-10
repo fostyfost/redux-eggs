555f0b500bb2b42d29b995406c6cf58c601d4544
Fosty Fost <fostyfost@gmail.com>
2021-05-10 11:20:18 +0300

    chore(yarn): switch to yarn v2

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  collectCoverageFrom: ['**!/!*.{js,jsx,ts,tsx}', '!**!/!*.d.ts', '!**!/node_modules/!**'],
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverageFrom: ['**!/!*.{js,jsx,ts,tsx}', '!**!/!*.d.ts', '!**!/node_modules/!**', '!**!/.yarn/!**'],
+  testPathIgnorePatterns: ['/node_modules/', '/.yarn/', '/.next/'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
-  transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
+  transformIgnorePatterns: ['/node_modules/', '/.yarn/', '^.+\\.module\\.(css|sass|scss)$'],
   // A map from regular expressions to module names that allow to stub out resources with a single module
   moduleNameMapper: {
     //  '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',

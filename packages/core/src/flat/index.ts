be6d7665d49552bf19d54bc81556e23e2c6dd048
Fosty Fost <fostyfost@gmail.com>
2021-11-11 17:30:53 +0300

    feat(@redux-eggs/core): refactor `flat`-util

--- packages/core/src/flat/index.ts
+++ packages/core/src/flat/index.ts
@@ -1,20 +1,22 @@
 import type { Egg, EggTuple } from '@/contracts'
 
+const isArray = Array.isArray
+
 export const flat = (tuple: EggTuple): Egg[] => {
   const result: Egg[] = []
 
-  const flat = (tuple: EggTuple): void => {
+  const flatten = (tuple: EggTuple): void => {
     tuple.forEach(item => {
-      if (Array.isArray(item)) {
-        flat(item)
+      if (isArray(item)) {
+        flatten(item)
       } else {
         result.push(item)
       }
     })
   }
 
-  if (Array.isArray(tuple)) {
-    flat(tuple)
+  if (isArray(tuple)) {
+    flatten(tuple)
   }
 
   return result
